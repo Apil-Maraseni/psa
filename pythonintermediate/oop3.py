8de9f6774317a920898528a95260be897c3a86d9
Apil-Maraseni <apil.maraseni5@gmail.com>
2025-06-16 22:20:38 +0545

    06/16

---
+++ pythonintermediate/oop3.py
@@ -0,0 +1,29 @@
+# # Topic: Implementing a Triangle Class to Calculate Perimeter
+
+# create the Triangle class
+class Triangle:
+    # define the __init__() method
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+
+    # define the get_perimeter() method
+    def get_perimeter(self):
+        return (self.x + self.y +self.z)
+
+# take three integer inputs
+a = int(input())
+b = int(input())
+c = int(input())
+
+# create an object of the Triangle class
+triangle1 = Triangle(a,b,c)
+
+# call the get_perimeter() method
+perimeter = triangle1.get_perimeter()
+
+# print the perimeter
+print(perimeter)
