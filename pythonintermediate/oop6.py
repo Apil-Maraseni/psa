d37191d35f9a04e1d24ee7d5cfa503c7d0e07ffe
Apil-Maraseni <apil.maraseni5@gmail.com>
2025-06-16 22:39:36 +0545

    06/16

---
+++ pythonintermediate/oop6.py
@@ -0,0 +1,17 @@
+# This code demonstrates inheritance in Python by creating a base class Animal
+
+# create the Animal class
+class Animal:
+    def eat(self):
+        print("I can eat food")
+
+# create the Dog class
+class Dog(Animal):
+    def bark(self):
+        print("I can bark")
+
+# create an object of Dog
+d1 = Dog()
+
+# call the eat() method using the object
+d1.eat()
