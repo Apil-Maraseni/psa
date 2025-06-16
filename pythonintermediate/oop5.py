8acc41ef2b7ed01a26086601132ba32b0e6da267
Apil-Maraseni <apil.maraseni5@gmail.com>
2025-06-16 22:31:32 +0545

    06/16

---
+++ pythonintermediate/oop5.py
@@ -0,0 +1,29 @@
+# This code demonstrates composition in Python by creating a Vehicle class that contains an Engine object.
+
+# create the Engine class
+class Engine:
+    # use __init__() to initialize the power attribute 
+    def __init__(self, power):
+        self.power = power
+
+# create the Vehicle class
+class Vehicle:
+    # use __init__() to initialize the wheels attribute
+ 
+    def __init__(self, wheels):
+        self.wheels = wheels
+        
+        # the engine attribute should be an object of the Engine class with the power attribute 400
+        self.engine = Engine(400)
+        
+    
+    # create the get_power() method
+    def get_power(self):
+        # print the power attribute of the engine attribute (which is an object of Engine) 
+        print (self.engine.power)
+
+# create an object of Vehicle
+v1 = Vehicle(4)
+
+# call the get_power() method using the object
+v1.get_power()
