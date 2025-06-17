1ceeaf08bcf3e734213ad32f8980819752d9e443
Apil-Maraseni <apil.maraseni5@gmail.com>
2025-06-17 21:41:25 +0545

    06/17

---
+++ pythonintermediate/exception2.py
@@ -0,0 +1,14 @@
+## Python program to demonstrate try-except block
+
+# create a try block
+try:
+    countries = {'Nepal' : 'Kathmandu', 'Sweden' : 'Stockholm', 'Italy' : 'Rome'}
+
+    # take string input
+    key = input()
+
+    # print the value present at key
+    print(countries[key])
+# create the except block
+except:
+    print("Key is not available.")
