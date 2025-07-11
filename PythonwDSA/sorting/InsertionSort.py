7f8a72688648f0b597f57901f218bc208673a34a
Apil Maraseni <apil.maraseni5@gmail.com>
2025-07-11 23:07:26 +0545

    7/11

---
+++ PythonwDSA/sorting/InsertionSort.py
@@ -0,0 +1,27 @@
+# #inserion sorting algorithm
+# take elements one by one and insert them in the right place in the sorted part of the array
+# # Time complexity: O(n^2)
+
+
+
+def insertion_sort(lst):
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i - 1
+
+        # compare the key element with elements to its left one by one
+        # end the loop if the key element is larger or equal
+        while j >= 0 and key <= lst[j]:
+            # shift the element to its right
+            lst[j + 1] = lst[j]
+            # decrease j to go to the next element to its left
+            j = j - 1
+
+        # insert the key element to the correct position
+        lst[j + 1] = key
+    return lst
+
+data = [7,4,1,3,2]
+print(f"unsorted list: {data}")
+sorted_list = insertion_sort(data)
+print(f"sorted list: {sorted_list}")
