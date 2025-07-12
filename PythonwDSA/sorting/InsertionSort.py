7cd07046cc3d57e8df290911e0c53cbcb95255b9
Apil Maraseni <apil.maraseni5@gmail.com>
2025-07-12 22:14:07 +0545

    7/12

--- PythonwDSA/sorting/InsertionSort.py
+++ PythonwDSA/sorting/InsertionSort.py
@@ -25,3 +25,25 @@ data = [7,4,1,3,2]
 print(f"unsorted list: {data}")
 sorted_list = insertion_sort(data)
 print(f"sorted list: {sorted_list}")
+
+# in descending order
+def insertion_sort_desc(lst):
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i - 1
+
+        # compare the key element with elements to its left one by one
+        # end the loop if the key element is smaller or equal
+        while j >= 0 and key >= lst[j]:
+            # shift the element to its right
+            lst[j + 1] = lst[j]
+            # decrease j to go to the next element to its left
+            j = j - 1
+
+        # insert the key element to the correct position
+        lst[j + 1] = key
+    return lst
+data = list(map(int, input().split()))
+sorted_list = insertion_sort_desc(data)
+print(f"sorted list in descending order: {sorted_list}")
+# Swap this element with the second element.
