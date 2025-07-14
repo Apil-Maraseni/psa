ec5dc8cd44383dcc3cf174617af0c7d29a82473b
Apil Maraseni <apil.maraseni5@gmail.com>
2025-07-14 23:36:22 +0545

    7/14

--- PythonwDSA/sorting/QuickSort.py
+++ PythonwDSA/sorting/QuickSort.py
@@ -23,3 +23,29 @@ lst = [7,2,1,6,8,5,3,4]
 print(f"unsorted list: {lst}")
 sorted_list = quick_sort(lst)
 print(f"sorted list: {sorted_list}")
+
+#descending order 
+
+def quick_sort(lst):
+    # write you code here    
+    length = len(lst)
+    if length <=1 :
+        return lst
+    else:
+        pivot = lst.pop()
+    
+    right =[]
+    left = []
+    for element in lst:
+        if element > pivot:
+            left.append(element)
+        else:
+            right.append(element)
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+# take integer inputs and convert it to a list
+data_list = list(map(int, input().split()))
+
+sorted_list = quick_sort(data_list)
+
+print(sorted_list)
