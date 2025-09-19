49eec689a2d9f27ef08f858fa637b2de569d828d
Apil Maraseni <apil.maraseni5@gmail.com>
2025-09-19 21:28:55 +0545

    9/19

---
+++ SQL/Chapter1/test.sql
@@ -0,0 +1,3 @@
+SELECT * FROM shows
+-- here * selects all the columns from the table shows but if we want only some portions then we ca just specify them as SELECT column1, column2, column3 and so on
+SELECT name, genre, FROM shows
