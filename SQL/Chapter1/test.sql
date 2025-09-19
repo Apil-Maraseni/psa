318181b5d5107b4e484d77dea2bbfe8f8bfc35fe
Apil Maraseni <apil.maraseni5@gmail.com>
2025-09-19 21:52:01 +0545

    9/19

--- SQL/Chapter1/test.sql
+++ SQL/Chapter1/test.sql
@@ -1,3 +1,18 @@
 SELECT * FROM shows
 -- here * selects all the columns from the table shows but if we want only some portions then we ca just specify them as SELECT column1, column2, column3 and so on
 SELECT name, genre, FROM shows
+-- to avooid dublicates we use the keyword distinct
+SELECT DISTINCT column_name
+FROM table_name;
+
+-- for filtering based on conditions we use the keyword WHERE
+SELECT *
+FROM shows
+WHERE id = 23;
+
+-- another eg 
+SELECT *
+FROM shows
+WHERE year > 2000
+
+-- we can uise the ewual not equal greater less greater equal to or less equal to and so on
---
+++ SQL/Chapter1/where.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM shows
+WHERE reviews > 60;
+
+-- it retrives all the reviews wich are greater than 60% in rotten tomatoes
